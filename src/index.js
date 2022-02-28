b5db5baadc7a16d8478b6f0aa2e3e3593329fa9f
ArturKD <arturkoshal@gmail.com>
2022-02-28 13:10:25 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports =
+    function reverse (n) {
+    if (Number(n) < 0) {
+        n = Number(n - n - n);
+    }
+  let arr = [];
+  let string = n.toString();
+  for (let i = 0; i < string.length; i ++) {
+      arr.push(string[i]);
+  }
+  return  arr.reverse().join('');
 }
+
